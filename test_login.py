fd40d77443995e9bfd181185ecd76c5e464117c3
hitheima <haoyuexing@itcast.cn>
2019-05-05 15:06:30 +0800

    删除 logout

--- test_login.py
+++ test_login.py
@@ -7,10 +7,6 @@ class TestLogin:
 	def test_login2(self):
 		print("test_login2")
 		assert 0
-
-	def test_logout():
-		print("test_login")
-		assert 1
 		
 	def test_login3(self):
 		print("test_login3")
