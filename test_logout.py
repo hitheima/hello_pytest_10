cce9e2188dac8912d0c62483fbee16f501ede00f
hitheima <hitheima@163.com>
2019-05-05 17:01:45 +0800

    修改了logout

--- test_logout.py
+++ test_logout.py
@@ -1,13 +1,4 @@
 class TestLogout:
 
-	def test_logout1(self):
-		print("test_logout1")
-		assert 1
-
-	def test_logout2(self):
-		print("test_logout2")
-		assert 0
-
-	def test_login3(self):
-		print("xxx")
-		assert 1
+    def test_logout(self):
+        print("123")
